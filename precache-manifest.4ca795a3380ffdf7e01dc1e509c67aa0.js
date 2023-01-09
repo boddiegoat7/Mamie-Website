c488e8d5bfaf3c31d3d95a3f3d38c787ab9408ca
victor boddie <victorboddie21@gmail.com>
2023-01-08 20:08:49 -0500

    Updates

---
+++ precache-manifest.4ca795a3380ffdf7e01dc1e509c67aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649c6a00d52183c4ed9218a9084fd478",
+    "url": "/church-site/index.html"
+  },
+  {
+    "revision": "482799a128cbc2e465d8",
+    "url": "/church-site/static/css/main.3b660e2d.chunk.css"
+  },
+  {
+    "revision": "8dfe3e6f7b4a2ce325ec",
+    "url": "/church-site/static/js/2.223f1624.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/church-site/static/js/2.223f1624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482799a128cbc2e465d8",
+    "url": "/church-site/static/js/main.7b08e1a5.chunk.js"
+  },
+  {
+    "revision": "dfad6e4501f5cc45ffb3",
+    "url": "/church-site/static/js/runtime-main.58e6f16e.js"
+  }
+]);
