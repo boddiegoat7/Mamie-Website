da52f0fc7b873144fc4d100153a004d320c0f886
boddiegoat7 <victorboddie21@gmail.com>
2023-04-16 20:14:00 -0400

    fixed sizing

--- mamie/src/components/Navbar/Navbar.js
+++ mamie/src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL
+            THE DIVINE CHURCH 
             <i class="fab fa-typo3" />
           </Link>
 
